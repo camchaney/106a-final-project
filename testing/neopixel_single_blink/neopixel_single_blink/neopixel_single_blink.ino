00dbf4a6eefc166e804d09723469d18781c9d841
camchaney <cameron.chaney.vssa@gmail.com>
2022-11-18 22:07:02 -0800

    got huzzah working with neopixel blink

--- testing/neopixel_single_blink/neopixel_single_blink/neopixel_single_blink.ino
+++ testing/neopixel_single_blink/neopixel_single_blink/neopixel_single_blink.ino
@@ -5,7 +5,7 @@
 #include <Adafruit_NeoPixel.h>
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        23 // On Trinket or Gemma, suggest changing this to 1
+#define PIN        13 // On Trinket or Gemma, suggest changing this to 1
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS 64 // Popular NeoPixel ring size
@@ -26,21 +26,23 @@ void setup() {
 }
 
 void loop() {
-  pixels.clear(); // Set all pixel colors to 'off'
+  //pixels.clear(); // Set all pixel colors to 'off' (for some reason this doesn't work with 1 neopixel)
+  pixels.setPixelColor(32, pixels.Color(0, 0, 0));
+  pixels.show();   // Send the updated pixel colors to the hardware.
   Serial.println("off");
-  //delay(DELAYVAL); // Pause before next pass through loop
+  delay(DELAYVAL); // Pause before next pass through loop
 
   // The first NeoPixel in a strand is 0, second is 1, all the way up
   // to the count of pixels minus one.
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+  //for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
 
-    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-    // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-  
-    pixels.show();   // Send the updated pixel colors to the hardware.
-    Serial.println("on");
-  
-    delay(DELAYVAL); // Pause before next pass through loop
-  }
+  // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+  // Here we're using a moderately bright green color:
+  pixels.setPixelColor(32, pixels.Color(0, 150, 0));
+
+  pixels.show();   // Send the updated pixel colors to the hardware.
+  Serial.println("on");
+
+  delay(DELAYVAL); // Pause before next pass through loop
+  //}
 }
