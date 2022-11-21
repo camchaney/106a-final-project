8167cc29e7c518b3de44ed7c0cfbd5410ee2bd32
camchaney <cameron.chaney.vssa@gmail.com>
2022-11-21 15:34:59 -0800

    adding firmware

--- testing/neopixel_single_blink/neopixel_single_blink/neopixel_single_blink.ino
+++ testing/neopixel_single_blink/neopixel_single_blink/neopixel_single_blink.ino
@@ -4,22 +4,23 @@
 
 #include <Adafruit_NeoPixel.h>
 
-// Which pin on the Arduino is connected to the NeoPixels?
+// Constants
 #define PIN        13 // On Trinket or Gemma, suggest changing this to 1
-
-// How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS 64 // Popular NeoPixel ring size
+const int pin_pot = A3;       // pin for potentiometer dimming
 
-// When setting up the NeoPixel library, we tell it how many pixels,
-// and which pin to use to send signals. Note that for older NeoPixel
-// strips you might need to change the third parameter -- see the
-// strandtest example for more information on possible values.
+
+
+// Object Initialization
+
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 #define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
 
 void setup() {
   Serial.begin(9600);
+  //pinMode(pin_pot, INPUT);
+  analogReadResolution(12);
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
   Serial.println("Pixel set up");
@@ -35,7 +36,10 @@ void loop() {
   // The first NeoPixel in a strand is 0, second is 1, all the way up
   // to the count of pixels minus one.
   //for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
-
+  // Read potentiometer value
+  int sensorValue = analogRead(pin_pot);
+  int mapValue = map(sensorValue, 0, 1, 0, 255);
+  Serial.println(digitalRead(mapValue));
   // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
   // Here we're using a moderately bright green color:
   pixels.setPixelColor(32, pixels.Color(0, 150, 0));
