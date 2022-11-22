0f8b02477d043721a250edf2e87c7f55f83e696b
Class Account <ee106a-ads@c105-9.local>
2022-11-21 17:00:41 -0800

    got circle to work

--- light_drawing/src/light_drawer/src/path_planner.py
+++ light_drawing/src/light_drawer/src/path_planner.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import sys
 import rospy
 import moveit_commander
 from moveit_msgs.msg import OrientationConstraint, Constraints, CollisionObject
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Pose
 from shape_msgs.msg import SolidPrimitive
 
 class PathPlanner(object):
@@ -25,12 +27,21 @@ class PathPlanner(object):
 
         self._planning_scene_publisher = rospy.Publisher('/collision_object', CollisionObject, queue_size=10)
 
-        #NEED TO FIND SOME SORT OF BOOLEAN MESSAGE TYPE
-        self._light_control_publisher = rospy.Publisher('/light_toggle', None, queue_size=10)
+        # #NEED TO FIND SOME SORT OF BOOLEAN MESSAGE TYPE
+        # self._light_control_publisher = rospy.Publisher('/light_toggle', None, queue_size=10)
 
         self._group = moveit_commander.MoveGroupCommander(group_name)
 
         self._group.set_workspace([-2, -2, -2, 2, 2, 2])
+
+        self.orien_const = OrientationConstraint()
+        self.orien_const.link_name = "right_gripper";
+        self.orien_const.header.frame_id = "base";
+        self.orien_const.orientation.y = -1.0;
+        self.orien_const.absolute_x_axis_tolerance = 0.1;
+        self.orien_const.absolute_y_axis_tolerance = 0.1;
+        self.orien_const.absolute_z_axis_tolerance = 0.1;
+        self.orien_const.weight = 1.0;
 
         rospy.sleep(0.5)
 
@@ -50,16 +61,18 @@ class PathPlanner(object):
         """
         Connects all the contours in the 'contours' list and adds filler points between contours
         """
-        new_contour_list = np.array([])
-        last_end = None
-        for contour in contours:
-            if last_end is not None:
-                beginning = contour[0]
-                slope = (beginning[1] - last_end[1]) / (beginning[0] - last_end[0])
-                for i in range(5 * int(self.distance(last_end, beginning))):
-                    np.append(new_contour_list, np.array(last_end[0] + i * .2, last_end[1] + i * .2 * slope))
-            np.append(new_contour_list, contour)
-            last_end = contour[-1]
+        return contours
+        # new_contour_list = np.array([])
+        # last_end = None
+        # print(contours)
+        # for contour in contours:
+        #     if last_end is not None:
+        #         beginning = contour[0]
+        #         slope = (beginning[1] - last_end[1]) / (beginning[0] - last_end[0])
+        #         for i in range(5 * int(self.distance(last_end, beginning))):
+        #             np.append(new_contour_list, np.array(last_end[0] + i * .2, last_end[1] + i * .2 * slope))
+        #     np.append(new_contour_list, contour)
+        #     last_end = contour[-1]
         
     def transform_contours(self, contours):
         """
@@ -69,14 +82,33 @@ class PathPlanner(object):
         #TODO
         return contours
 
-    def plan_along_path(self, contours, orientation_constraints):
+    def ndarray_to_pos(self, arr):
+        """
+        Creates an array of pose messages from an ndarray
+        """
+        poses = []
+        for i in range(arr.shape[1]):
+            pos = Pose()
+            pos.position.x = arr[0][i]
+            pos.position.y = arr[1][i]
+            pos.position.z = arr[2][i]
+            pos.orientation.x = 0
+            pos.orientation.y = 0.7071068
+            pos.orientation.z = 0
+            pos.orientation.w = -0.7071068
+            poses.append(pos)
+        return poses
+
+    def plan_along_path(self, contours, orientation_constraints=None):
         """
         Plans a path for the robot given contours to draw and the end effector orientation constraints
         """
-        constraints = Constraints()
-        constraints.orientation_constraints = orientation_constraints
-        self._group.set_path_constraints(constraints)
+        if orientation_constraints is not None:
+            constraints = Constraints()
+            constraints.orientation_constraints = orientation_constraints
+            self._group.set_path_constraints(constraints)
         connected_contours = self.connect_contours(contours)
+        connected_contours = self.ndarray_to_pos(connected_contours)
         return self._group.compute_cartesian_path(connected_contours, .01, 0)
 
     def execute_plan(self, plan):
@@ -84,3 +116,6 @@ class PathPlanner(object):
         Executes the planned path
         """
         return self._group.execute(plan, True)
+
+    
+
--- light_drawing/src/light_drawer/src/test.py
+++ light_drawing/src/light_drawer/src/test.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python3
+
 import os
 import cv2
+import numpy as np
+import rospy
+from intera_interface import Limb
 from feature_extractor import FeatureExtractor
-# from path_planner import PathPlanner
+from path_planner import PathPlanner
+from controller import Controller
 
 
 if __name__=="__main__":
+    rospy.init_node('moveit_node')
     cwd = os.path.realpath(os.path.dirname(__file__))
     files = [filename for filename in os.listdir(cwd + "/images") if ".jpg" in filename]
     print("\n\nInput integer of image you want: \n")
@@ -24,4 +31,19 @@ if __name__=="__main__":
     cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 1)
     feature_extractor.draw_image(feature_extractor.invert_black_white(contour_img), "contours")
 
-    
+    path_planner = PathPlanner()
+    input_coords = np.arange(0, 2 * np.pi, .1)
+    simple_circle = .1 * np.array((np.cos(input_coords), 0.2 + np.sin(input_coords)))
+    t1 = np.ones(simple_circle[0].shape).T.reshape((1, simple_circle.shape[1])) * .75
+    t2 = simple_circle
+    simple_circle = np.vstack((t1, t2))
+    plan, _ = path_planner.plan_along_path(simple_circle)
+    input("check rviz bruh")
+    Kp = 0.2 * np.array([0.4, 2, 1.7, 1.5, 2, 2, 3])
+    Kd = 0.01 * np.array([2, 1, 2, 0.5, 0.8, 0.8, 0.8])
+    Ki = 0.01 * np.array([1.4, 1.4, 1.4, 1, 0.6, 0.6, 0.6])
+    Kw = np.array([0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9])
+    controller = Controller(Kp,Kd,Ki,Kw, Limb("right"))
+    controller.execute_plan(plan)
+    # connected_contours = path_planner.connect_contours(contours)
+    # cv2.drawContours(connect_contours)
