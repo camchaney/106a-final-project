02b36cad1ae71cd8e2b84c48825d1a6886e06302
Class Account <ee106a-ads@c105-10.local>
2022-11-21 19:29:28 -0800

    got basic images to work

--- light_drawing/src/light_drawer/src/path_planner.py
+++ light_drawing/src/light_drawer/src/path_planner.py
@@ -57,22 +57,32 @@ class PathPlanner(object):
         """
         return abs(((x[0] - y[0])**2 + (x[1] - y[1])**2)**.5)
 
+    def sample(self, contours, rate):
+        new_contour_list = []
+        for i, contour in enumerate(contours):
+            if i % rate == 0:
+                new_contour_list.append(contour)
+        return np.array(new_contour_list)
+
     def connect_contours(self, contours):
         """
         Connects all the contours in the 'contours' list and adds filler points between contours
         """
-        return contours
-        # new_contour_list = np.array([])
-        # last_end = None
-        # print(contours)
-        # for contour in contours:
-        #     if last_end is not None:
-        #         beginning = contour[0]
-        #         slope = (beginning[1] - last_end[1]) / (beginning[0] - last_end[0])
-        #         for i in range(5 * int(self.distance(last_end, beginning))):
-        #             np.append(new_contour_list, np.array(last_end[0] + i * .2, last_end[1] + i * .2 * slope))
-        #     np.append(new_contour_list, contour)
-        #     last_end = contour[-1]
+        new_contour_list = []
+        last_end = None
+        for contour in contours:
+            contour = np.squeeze(contour, axis=1)
+            if last_end is not None:
+                beginning = contour[0]
+                slope = (beginning[1] - last_end[1]) / (beginning[0] - last_end[0])
+                for i in range(5 * int(self.distance(last_end, beginning))):
+                    np.append(new_contour_list, np.array(last_end[0] + i * .2, last_end[1] + i * .2 * slope))
+            for point in contour:
+                new_contour_list.append(point)
+            last_end = contour[-1]
+        np_contours = np.array(new_contour_list)
+        print(np_contours.shape)
+        return np_contours
         
     def transform_contours(self, contours):
         """
@@ -80,7 +90,16 @@ class PathPlanner(object):
         returns: a single leve list of all the contour coordinates in world frame coordinates 
         """
         #TODO
-        return contours
+        contours = contours.T
+        transformation = np.array([
+            [0, 0, .7],
+            [.8 / 500, 0, -.4],
+            [0, -.8 / 500, .8],
+        ])
+        dim = contours.shape[1]
+        unos = np.ones((1, dim)).reshape((1, dim))
+        homo_coords = np.vstack((contours, unos))
+        return transformation @ homo_coords
 
     def ndarray_to_pos(self, arr):
         """
@@ -92,23 +111,30 @@ class PathPlanner(object):
             pos.position.x = arr[0][i]
             pos.position.y = arr[1][i]
             pos.position.z = arr[2][i]
-            pos.orientation.x = 0
-            pos.orientation.y = 0.7071068
+            pos.orientation.x = 1
+            pos.orientation.y = 0
             pos.orientation.z = 0
-            pos.orientation.w = -0.7071068
+            pos.orientation.w = 0
             poses.append(pos)
+        print(f"Number of poses: {len(poses)}")
         return poses
 
     def plan_along_path(self, contours, orientation_constraints=None):
         """
         Plans a path for the robot given contours to draw and the end effector orientation constraints
         """
-        if orientation_constraints is not None:
+        if orientation_constraints is not None:                
+            slope = (beginning[1] - last_end[1]) / (beginning[0] - last_end[0])
             constraints = Constraints()
             constraints.orientation_constraints = orientation_constraints
             self._group.set_path_constraints(constraints)
         connected_contours = self.connect_contours(contours)
+        for p in connected_contours:
+            print(p)
+        # print(connected_contours)
+        connected_contours = self.transform_contours(connected_contours)
         connected_contours = self.ndarray_to_pos(connected_contours)
+        connected_contours = self.sample(connected_contours, 3)
         return self._group.compute_cartesian_path(connected_contours, .01, 0)
 
     def execute_plan(self, plan):
--- light_drawing/src/light_drawer/src/test.py
+++ light_drawing/src/light_drawer/src/test.py
@@ -28,16 +28,20 @@ if __name__=="__main__":
     contour_img = feature_extractor.create_empty_img(x, y)
     contours, hierarchy = feature_extractor.extract_contour(img)
     contours = feature_extractor.filter_contours_by_len(contours)
+    for contour in contours:
+        print(contour.squeeze(axis=1).shape)
     cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 1)
     feature_extractor.draw_image(feature_extractor.invert_black_white(contour_img), "contours")
 
     path_planner = PathPlanner()
-    input_coords = np.arange(0, 2 * np.pi, .1)
-    simple_circle = .1 * np.array((np.cos(input_coords), 0.2 + np.sin(input_coords)))
-    t1 = np.ones(simple_circle[0].shape).T.reshape((1, simple_circle.shape[1])) * .75
-    t2 = simple_circle
-    simple_circle = np.vstack((t1, t2))
-    plan, _ = path_planner.plan_along_path(simple_circle)
+    # input_coords = np.arange(0, 2 * np.pi, .1)
+    # simple_circle = .1 * np.array((np.cos(input_coords), 0.2 + np.sin(input_coords)))
+    # t1 = np.ones(simple_circle[0].shape).T.reshape((1, simple_circle.shape[1])) * .75
+    # t2 = simple_circle
+    # simple_circle = np.vstack((t1, t2))
+    # plan, _ = path_planner.plan_along_path(simple_circle)
+    plan, _ = path_planner.plan_along_path(contours)
+    # print(connected_contours.shape)
     input("check rviz bruh")
     Kp = 0.2 * np.array([0.4, 2, 1.7, 1.5, 2, 2, 3])
     Kd = 0.01 * np.array([2, 1, 2, 0.5, 0.8, 0.8, 0.8])
@@ -45,5 +49,4 @@ if __name__=="__main__":
     Kw = np.array([0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9])
     controller = Controller(Kp,Kd,Ki,Kw, Limb("right"))
     controller.execute_plan(plan)
-    # connected_contours = path_planner.connect_contours(contours)
-    # cv2.drawContours(connect_contours)
+    
