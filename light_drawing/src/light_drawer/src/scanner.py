6e2f60df760ae536d3c6955ad2361bf4fd6fd032
adam.rashid96 <abrashid@berkeley.edu>
2022-12-05 15:45:39 -0800

    color contours

--- light_drawing/src/light_drawer/src/scanner.py
+++ light_drawing/src/light_drawer/src/scanner.py
@@ -1,4 +1,5 @@
 from skimage.filters import threshold_local
+from feature_extractor import FeatureExtractor
 import numpy as np
 import argparse
 import cv2
@@ -60,14 +61,39 @@ def four_point_transform(image, pts):
 if __name__=="__main__":
     # construct the argument parser and parse the arguments
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required = True,
+    feature_extractor = FeatureExtractor()
+    ap.add_argument("-i", "--image", required = False,
         help = "Path to the image to be scanned")
     args = vars(ap.parse_args())
-
-
-    # load the image and compute the ratio of the old height
-    # to the new height, clone it, and resize it
-    image = cv2.imread(args["image"])
+    img_name = ""
+    if args["image"] is None:
+        print("STEP 0: WEBCAM")
+        cv2.namedWindow("preview")
+        vc = cv2.VideoCapture(0)
+        if vc.isOpened(): # try to get the first frame
+            rval, frame = vc.read()
+        else:
+            rval = False
+        while rval:
+            cv2.imshow("preview", frame)
+            rval, frame = vc.read()
+            key = cv2.waitKey(1)
+            if key == 27: # exit on ESC
+                break
+            if key == 13:
+                # Enter pressed
+                img_name = "images/webcam.png"
+                cv2.imwrite(img_name, frame)
+                print("Image taken!")
+                break
+        vc.release()
+        cv2.destroyWindow("preview")
+    else:
+        # load the image and compute the ratio of the old height
+        # to the new height, clone it, and resize it
+        img_name = args["image"]
+    
+    image = cv2.imread(img_name)
     ratio = image.shape[0] / 500.0
     orig = image.copy()
     image = imutils.resize(image, height = 500)
@@ -87,6 +113,29 @@ if __name__=="__main__":
     # largest ones, and initialize the screen contour
     cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
+    colors = feature_extractor.extract_color(image, cnts)
+
+    colored_image = np.zeros(image.shape)
+    for i, contour in enumerate(cnts):
+        print((colors[i][0], colors[i][1], colors[i][2]))
+        cv2.drawContours(colored_image, [contour], -1, (colors[i][0], colors[i][1], colors[i][2]), 2)
+
+    # print("DRAWING COLORED CONTOURS")
+    # colored_image = np.zeros(image.shape)
+    # for i, contour in enumerate(cnts):
+    #     contour = contour.squeeze(1)
+    #     # print((colors[i][0], colors[i][1], colors[i][2]))
+    #     for point in contour:
+    #         x, y = point[0], point[1]
+    #         colored_image[y][x][0] = colors[i][0]
+    #         colored_image[y][x][1] = colors[i][1]
+    #         colored_image[y][x][2] = colors[i][2]
+    #         # colored_image[y][x][0] = 255
+    #         # colored_image[y][x][1] = 0
+    #         # colored_image[y][x][2] = 0
+    # print(colored_image.shape, colored_image[:, :, 0].max(),  colored_image[:, :, 0].min())
+    cv2.imwrite("test.png", colored_image)
+
     cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
     # loop over the contours
     for c in cnts:
@@ -110,10 +159,12 @@ if __name__=="__main__":
     warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
     # convert the warped image to grayscale, then threshold it
     # to give it that 'black and white' paper effect
-    warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-    T = threshold_local(warped, 11, offset = 10, method = "gaussian")
-    warped = (warped > T).astype("uint8") * 255
+    
+    # warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+    # T = threshold_local(warped, 11, offset = 10, method = "gaussian")
+    # warped = (warped > T).astype("uint8") * 255
     # show the original and scanned images
+    
     print("STEP 3: Apply perspective transform")
     cv2.imshow("Original", imutils.resize(orig, height = 650))
     cv2.imshow("Scanned", imutils.resize(warped, height = 650))
